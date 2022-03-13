5e02378507c3ec27635143b8636d068761bbe4f6
pansouk <pansouk@gmail.com>
2022-03-13 16:38:09 +0200

    return html and 404 templates

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
+const path = require('path')
+
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/add-product', (req, res, next) => {
     console.log('To the next middleware')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
+const path = require('path')
+
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    console.log('To the next middleware')
-    res.send('<h1>Hello from express!!</h1>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 })
 
 
