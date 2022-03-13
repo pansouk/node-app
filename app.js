3a49b69fad3f911d015ec92afd125655c0c48f84
pansouk <pansouk@gmail.com>
2022-03-13 15:20:50 +0200

    Add body-parser package

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 // Node specific modules
-const http = require('http')
+
 
 
 // Packages modules
 const express = require('express')
+const bodyParser = require('body-parser')
 
 
 // Create an express application
 const app = new express()
 
-// Middlewares are a basic consept in express js
-app.use((req, res, next) => {
-    console.log('In the middleware')
-    next(); // Allows the request to continue to the next middleware
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product', (req, res, next) => {
+    console.log('To the next middleware')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+  res.redirect('/')  
+})
+
+app.use('/',(req, res, next) => {
     console.log('To the next middleware')
     res.send('<h1>Hello from express!!</h1>')
 })
 
-//  app is a valid request handler to construct the server.
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000)
