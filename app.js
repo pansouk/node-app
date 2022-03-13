474be4ec04e62c37b7fb667edfa7f975a499bfd9
pansouk <pansouk@gmail.com>
2022-03-13 16:02:57 +0200

    404 response

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Node specific modules
 
 
-
 // Packages modules
 const express = require('express')
 
@@ -19,5 +18,9 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(adminRoutes)
 
 app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!!!</h1>')
+})
 
 app.listen(3000)
