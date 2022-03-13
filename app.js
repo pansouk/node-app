9a9c7933a63243e63abb4c0d76c87264f9affac0
pansouk <pansouk@gmail.com>
2022-03-13 21:51:15 +0200

    Pug template engine

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 // Packages modules
 const express = require('express')
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const bodyParser = require('body-parser')
@@ -13,17 +13,21 @@ const bodyParser = require('body-parser')
 // Create an express application
 const app = new express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Access static files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin',adminRoutes)
+app.use('/admin',adminData.routes)
 
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.render('404', {pageTitle: 'Page not found'})
 })
 
 app.listen(3000)
