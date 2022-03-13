efd1f09ccf03b985b5dfeee6f7375e8548d23ac4
pansouk <pansouk@gmail.com>
2022-03-13 14:52:33 +0200

    express middleware

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const express = require('express')
 // Create an express application
 const app = new express()
 
+// Middlewares are a basic consept in express js
+app.use((req, res, next) => {
+    console.log('In the middleware')
+    next(); // Allows the request to continue to the next middleware
+})
+
+app.use((req, res, next) => {
+    console.log('To the next middleware')
+    res.send('<h1>Hello from express!!</h1>')
+})
+
 //  app is a valid request handler to construct the server.
 const server = http.createServer(app);
 
