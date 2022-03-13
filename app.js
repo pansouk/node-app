77f54af6cd3929223837850aa2c8571ed09f2bd8
pansouk <pansouk@gmail.com>
2022-03-13 14:37:40 +0200

    install express.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+// Node specific modules
 const http = require('http')
-const routes = require('./routes')
 
 
-const server = http.createServer(routes);
+// Packages modules
+const express = require('express')
+
+
+// Create an express application
+const app = new express()
+
+//  app is a valid request handler to construct the server.
+const server = http.createServer(app);
 
 server.listen(3000);
